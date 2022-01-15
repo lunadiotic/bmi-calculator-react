7bee4a4765774a8f6725043ac9c195140316ee6f
lionatic <ahmadhakim404@gmail.com>
2022-01-15 12:13:56 +0700

    show result bmi with color notification

--- src/components/Bmi.js
+++ src/components/Bmi.js
@@ -3,6 +3,39 @@ import BmiCalculator from './BmiCalculator'
 
 const Bmi = () => {
     const [bmiValue, setBmiValue] = useState(0)
+
+    const getBmiResult = (bmi) => {
+        if (bmi >= 1 && bmi <= 18.5) {
+            return {
+                status: 'Underweight',
+                color: 'danger'
+            }
+        }
+        if (bmi >= 18.5 && bmi <= 24.9) {
+            return {
+                status: 'Normal',
+                color: 'success'
+            }
+        }
+        if (bmi >= 24.9 && bmi <= 29.9) {
+            return {
+                status: 'Overweight',
+                color: 'warning'
+            }
+        }
+        if (bmi >= 30) {
+            return {
+                status: 'Obese',
+                color: 'danger'
+            }
+        }
+        return {
+            status: '-',
+            color: 'primary' 
+        }
+    }
+
+    const bmiResult = getBmiResult(bmiValue)
     
     return (
         <>
@@ -15,8 +48,8 @@ const Bmi = () => {
                         <span>Body Mass Index (BMI) = {bmiValue}</span>
                         <BmiCalculator 
                             getBmiValue={setBmiValue} />
-                        <div className="card-footer bg-primary text-white">
-                            -
+                        <div className={`card-footer bg-${bmiResult.color} text-white`}>
+                            {bmiResult.status}
                         </div>
                     </div>
                 </div>
